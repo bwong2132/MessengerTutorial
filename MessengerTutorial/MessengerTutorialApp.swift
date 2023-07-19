6e879bd8126201075c271e679dc56dbf20703684
Bernie Wong <bwong2132@gmail.com>
2023-07-19 11:38:04 -0400

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Bernie Wong on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
